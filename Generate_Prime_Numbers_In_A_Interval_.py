bec064f9f9cc9fe596ee72c035d6ba78fafd882c
MummidiLakshman <96734843+MummidiLakshman@users.noreply.github.com>
2022-06-01 19:43:42 +0530

    def prime(a):     if(a==1):         return 0     for i in range(2,(a//2)+1):         if(a%i==0):             return 0     return 1 a=int(input()) b=int(input()) for i in range(a,b+1):     if(prime(i)):         print(i)

--- Generate_Prime_Numbers_In_A_Interval_.py
+++ Generate_Prime_Numbers_In_A_Interval_.py
@@ -1,7 +1,7 @@
 def prime(a):
     if(a==1):
         return 0
-    for i in range(2,int(a**0.5)+1):
+    for i in range(2,(a//2)+1):
         if(a%i==0):
             return 0
     return 1
@@ -10,3 +10,4 @@ b=int(input())
 for i in range(a,b+1):
     if(prime(i)):
         print(i)
+ 
