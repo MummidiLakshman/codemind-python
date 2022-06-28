f9b82bb2db78159bf2a79fdf74ddf415d190e399
MummidiLakshman <96734843+MummidiLakshman@users.noreply.github.com>
2022-06-28 22:16:49 +0530

    n=int(input()) g=n d=0 while(n!=0):     s=n%10     d=d*10+s     n=n//10 if(g==d):     print('True') else:     print('False')

--- Palindrome.py
+++ Palindrome.py
@@ -1,12 +1,11 @@
 n=int(input())
-s=0
-b=0
-t=n
-while(n>0):
+g=n
+d=0
+while(n!=0):
     s=n%10
-    b=b*10+s
+    d=d*10+s
     n=n//10
-if(t==b):
+if(g==d):
     print('True')
 else:
     print('False')
