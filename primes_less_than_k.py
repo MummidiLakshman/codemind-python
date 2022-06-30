36cf6b6e72c6b279d3c2618b1e000eb24731013d
MummidiLakshman <96734843+MummidiLakshman@users.noreply.github.com>
2022-06-30 21:42:16 +0530

    def prime(a):     if(a==1):         return 0     for i in range(2,(a//2)+1):         if(a%i==0):             return 0     else:         return 1 a=int(input()) b=list(map(int,input().split())) k=int(input()) c=0 for i in b:     if(prime(i)):         if(i<=k):             c+=1 print(c)

--- primes_less_than_k.py
+++ primes_less_than_k.py
@@ -1,13 +1,11 @@
 def prime(a):
-    c=0
     if(a==1):
         return 0
     for i in range(2,(a//2)+1):
         if(a%i==0):
             return 0
     else:
-        c+=1
-        return c
+        return 1
 a=int(input())
 b=list(map(int,input().split()))
 k=int(input())
