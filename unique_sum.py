93c4f01129cf2a69296ad0196886776af2cae33e
MummidiLakshman <96734843+MummidiLakshman@users.noreply.github.com>
2022-07-10 13:09:02 +0530

    a=int(input()) b=list(map(int,input().split())) k=[] f=0 for i in b:     if i not in k:         k.append(i) for i in k:     f+=i print(f)

--- unique_sum.py
+++ unique_sum.py
@@ -1,10 +1,10 @@
 a=int(input())
 b=list(map(int,input().split()))
-s=[]
-c=0
+k=[]
+f=0
 for i in b:
-    if i not in s:
-        s.append(i)
-for i in s:
-    c+=i
-print(c)
+    if i not in k:
+        k.append(i)
+for i in k:
+    f+=i
+print(f)
