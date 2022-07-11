cdd16fd44c7ada271929d79870fd9084e32b2ca0
MummidiLakshman <96734843+MummidiLakshman@users.noreply.github.com>
2022-07-11 14:31:22 +0530

    a,b=map(int,input().split()) c=list(map(int,input().split())) d=list(map(int,input().split())) f=[] g=[] h=0 p=0 for i in c:     if i not in f:         f.append(i) for i in d:     if i not in g:         g.append(i) for i in f:     if i not in g:         h+=1 for i in g:     if i not in f:         p+=1 s=h+p print(s)

--- Count_elements_II.py
+++ Count_elements_II.py
@@ -1,17 +1,21 @@
 a,b=map(int,input().split())
 c=list(map(int,input().split()))
 d=list(map(int,input().split()))
-k=[]
-s=[]
-t=0
+f=[]
+g=[]
+h=0
+p=0
 for i in c:
-    if i not in d:
-        k.append(i)
-for j in d:
-    if j not in c:
-        k.append(j)
-for i in k:
-    if i not in s:
-        s.append(i)
-        t+=1
-print(t)
+    if i not in f:
+        f.append(i)
+for i in d:
+    if i not in g:
+        g.append(i)
+for i in f:
+    if i not in g:
+        h+=1
+for i in g:
+    if i not in f:
+        p+=1
+s=h+p
+print(s)
