246ddcb19f92950a7ba8bff4d9298d17d7d5dd2e
MummidiLakshman <96734843+MummidiLakshman@users.noreply.github.com>
2022-08-24 17:36:34 +0530

    n=input() n=n.lower() c=0 for i in n.split():     s=i[::-1]     if(s==i):         c+=1 print(c)

--- Count_Palindrome_words_in_a_string.py
+++ Count_Palindrome_words_in_a_string.py
@@ -1,16 +1,8 @@
-def rev(a):
-    c=0
-    s=a[::-1]
-    if(s==a):
-        return 1
-    else:
-        return 0
-a=input()
-a=a.lower()
-k=a.split()
+n=input()
+n=n.lower()
 c=0
-for i in k:
-    if(rev(i)):
+for i in n.split():
+    s=i[::-1]
+    if(s==i):
         c+=1
 print(c)
-        
