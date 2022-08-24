08f39ddf4631a82e4f0d6b2817f2d293e52b3dc5
MummidiLakshman <96734843+MummidiLakshman@users.noreply.github.com>
2022-08-24 18:23:38 +0530

    n=input() n=n.lower() a='abcdefghijklmnopqrstuvwxyz' c='' d=0 s='' for i in n:     if i in a:         s+=i for i in s:     if i not in c:         c+=i for i in c:     d+=1 if(d==26):     print('True') else:     print('False')

--- Pangram_or_not.py
+++ Pangram_or_not.py
@@ -1,11 +1,19 @@
-a=input()
-a=a.lower()
-b='abcdefghijklmnopqrstuvwxyz'
-for i in b:
-    if i not in a:
-        print('False')
-        break
-else:
+n=input()
+n=n.lower()
+a='abcdefghijklmnopqrstuvwxyz'
+c=''
+d=0
+s=''
+for i in n:
+    if i in a:
+        s+=i
+for i in s:
+    if i not in c:
+        c+=i
+for i in c:
+    d+=1
+if(d==26):
     print('True')
-        
+else:
+    print('False')
     
