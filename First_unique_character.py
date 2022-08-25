328c561d708a6f09eb9650d22a580053eec393ca
MummidiLakshman <96734843+MummidiLakshman@users.noreply.github.com>
2022-08-25 14:49:41 +0530

    n=input() n=n.lower() s=[] c=0 for i in n:     if n.count(i)==1:         s.append(i)         c+=1 if(c==0):     print('-1') else:     print(s[0])

--- First_unique_character.py
+++ First_unique_character.py
@@ -1,9 +1,12 @@
-a=input()
+n=input()
+n=n.lower()
+s=[]
 c=0
-for i in a:
-    if a.count(i)==1:
-            c=1
-            print(i)
-            break
+for i in n:
+    if n.count(i)==1:
+        s.append(i)
+        c+=1
 if(c==0):
     print('-1')
+else:
+    print(s[0])
