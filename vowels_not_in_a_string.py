6eda92aade2ba26daf6577a7f486396daec501b0
MummidiLakshman <96734843+MummidiLakshman@users.noreply.github.com>
2022-08-27 11:56:52 +0530

    n=input() n=n.lower() v='aeiou' k='' c=0 for i in n:     if i in v:         k+=i for i in v:     if i not in k:         print(i,end=' ')         c+=1 if(c==0):     print('0')

--- vowels_not_in_a_string.py
+++ vowels_not_in_a_string.py
@@ -1,12 +1,14 @@
 n=input()
 n=n.lower()
 v='aeiou'
-s=''
+k=''
 c=0
+for i in n:
+    if i in v:
+        k+=i
 for i in v:
-    if i not in n:
+    if i not in k:
         print(i,end=' ')
         c+=1
 if(c==0):
     print('0')
-
