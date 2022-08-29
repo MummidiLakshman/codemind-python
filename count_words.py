cd9a982e470e1283f74712f27b06ef807384e68e
MummidiLakshman <96734843+MummidiLakshman@users.noreply.github.com>
2022-08-29 14:36:48 +0530

    n=input() n=n.split() v='aeiouAEIOU' c=0 for i in n:     if (i[0]) in v and i[-1] not in v:         c+=1 print(c)

--- count_words.py
+++ count_words.py
@@ -1,18 +1,8 @@
 n=input()
-n=n.lower()
-s=n.split()
-v='aeiou'
-c='bcdfghjklmnpqrstvwxyz'
-h=0
-f=0
-for i in s:
-    for j in i:
-        k=j
-        break
-    for j in i[::-1]:
-        g=j
-        break
-    if k in v :
-        if g in c:
-            h+=1
-print(h)
+n=n.split()
+v='aeiouAEIOU'
+c=0
+for i in n:
+    if (i[0]) in v and i[-1] not in v:
+        c+=1
+print(c)
