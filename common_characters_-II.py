05918f673afac9151d02a85426b85f7c940faa94
MummidiLakshman <96734843+MummidiLakshman@users.noreply.github.com>
2022-08-29 11:34:18 +0530

    n=input() n=n.lower() m=input() m=m.lower() c=[] p=[] h='' o=0 for i in n:     if(i==' '):         continue     if i in m:         c.append(i) for i in m:     if(i==' '):         continue     if i in n:         c.append(i) for i in c:     if i not in p:         p.append(i)         o+=1 p.sort() print(o)

--- common_characters_-II.py
+++ common_characters_-II.py
@@ -2,24 +2,24 @@ n=input()
 n=n.lower()
 m=input()
 m=m.lower()
-k=[]
+c=[]
 p=[]
+h=''
+o=0
 for i in n:
     if(i==' '):
         continue
     if i in m:
-        k.append(i)
-for j in m:
+        c.append(i)
+for i in m:
     if(i==' '):
         continue
-    if j in n:
-        k.append(j)
-#print(k)
-for i in k:
-    if(i==' '):
-        continue
+    if i in n:
+        c.append(i)
+for i in c:
     if i not in p:
         p.append(i)
-#print(p)
-s=len(p)
-print(s)
+        o+=1
+p.sort()
+print(o)
+    
